356cde2c3fe33d27ca0655e9f0ae2fc173de0e19
SeniorCod-72 <wwekesa72@gmail.com>
2025-04-26 19:01:27 +0300

    service model implemented

---
+++ app/models/Service.py
@@ -0,0 +1,8 @@
+from app import db
+
+
+class Service(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
